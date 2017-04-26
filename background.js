769069e2756de0ebff29b81f5c9983390acce5f0
Petter Wallin <wallin.petter@gmail.com>
2017-04-27 00:01:03 +0200

    Removes cookies on addon-click and before running out of free articles.

--- background.js
+++ background.js
@@ -1,31 +1,55 @@
 var cookieToListenTo = "svd-mc";
-
+var domain = "svd.se";
+var numFreeArticles = 6;
 
 chrome.browserAction.onClicked.addListener(removeCookies);
 
 function removeCookies() {
-	chrome.cookies.getAll({domain: "svd.se"}, function(cookies) {
-		console.log(cookies);
+	chrome.cookies.getAll({domain: domain}, function(cookies) {
+		
+		var iterate = $.each(cookies, function(index,cookie){
+			console.log(cookie);
+			
+			chrome.cookies.remove({url: "https://www.svd.se", name: cookie.name}, function(result) {
+				console.log(result);
+			});		
+		});
+		
+		$.when(iterate).then(function(){updateBadgeCounter()}, function(){console.log("Error removing cookies")}); 
 	});
+
+
+	
 }
 
 jQuery(document).ready(function(){
 	console.log("ready");
 
-	chrome.cookies.get({url: "https://www.svd.se", name: "svd-mc"}, function(cookie) {
-		console.log(cookie);
-		chrome.browserAction.setBadgeText({text: cookie.value});
-	});
+	updateBadgeCounter();
 
 	chrome.cookies.onChanged.addListener(function (changeInfo) {
 		if(changeInfo.cookie.name == cookieToListenTo) {
 			var tmpValue = changeInfo.cookie.value;
 			console.log(tmpValue);
 			chrome.browserAction.setBadgeText({text: tmpValue});
-			if(tmpValue >= 19) {
-							
+			if(tmpValue >= (numFreeArticles-1)) {
+				removeCookies();		
 			}
 		}
     });
 
 });
+
+function updateBadgeCounter(){
+	
+	chrome.cookies.get({url: "https://www.svd.se", name: cookieToListenTo}, function(cookie) {
+		console.log(cookie);
+		var count = '0';			
+
+		if(cookie !=null){
+			count = cookie.value;
+		}
+
+		chrome.browserAction.setBadgeText({text: count});
+	});
+};
