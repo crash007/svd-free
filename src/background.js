0a0aceed55c6650f42579544ddcd3f1b6d14d45f
Petter Wallin <Petter Wallin>
2017-05-08 15:30:46 +0200

    new icon clear cookies after 4 articles show message when clearing cookies

--- src/background.js
+++ src/background.js
@@ -1,24 +1,20 @@
 var cookieToListenTo = "svd-mc";
 var domain = "svd.se";
-var numFreeArticles = 6;
+var numArticles = 4; //number of articles before warning that free articles are soon used up. (6 articles are free) 
 
-chrome.browserAction.onClicked.addListener(removeCookies);
-
-function removeCookies() {
+function removeCookies() {	
 	chrome.cookies.getAll({domain: domain}, function(cookies) {
 		
-		var iterate = $.each(cookies, function(index,cookie){
-			console.log(cookie);
-			
+		var iterate = $.each(cookies, function(index,cookie){			
 			chrome.cookies.remove({url: "https://www.svd.se", name: cookie.name}, function(result) {
-				console.log(result);
+				
 			});		
 		});
 		
-		$.when(iterate).then(function(){updateBadgeCounter()}, function(){console.log("Error removing cookies")}); 
+		$.when(iterate).then(function(){
+			updateBadgeCounter();
+		}, function(){console.log("Error removing cookies")}); 
 	});
-
-
 	
 }
 
@@ -31,9 +27,10 @@ jQuery(document).ready(function(){
 		if(changeInfo.cookie.name == cookieToListenTo) {
 			var tmpValue = changeInfo.cookie.value;
 			console.log(tmpValue);
-			chrome.browserAction.setBadgeText({text: (numFreeArticles-tmpValue).toString()});
-			if(tmpValue >= (numFreeArticles-1)) {
-				removeCookies();		
+			chrome.browserAction.setBadgeText({text: (numArticles-tmpValue).toString()});
+			if(tmpValue >= (numArticles-1)) {
+				chrome.browserAction.setBadgeText({text: "Clear"});
+				setTimeout(function(){removeCookies()},500);
 			}
 		}
     });
@@ -42,15 +39,14 @@ jQuery(document).ready(function(){
 
 function updateBadgeCounter(){
 	
-	chrome.cookies.get({url: "https://www.svd.se", name: cookieToListenTo}, function(cookie) {
-		console.log(cookie);
+	chrome.cookies.get({url: "https://www.svd.se", name: cookieToListenTo}, function(cookie) {		
 		var count = 0;			
 
 		if(cookie !=null){
 			count = cookie.value;
 		}
 		
-		var badgeText = numFreeArticles - count;
+		var badgeText = numArticles - count;
 
 		chrome.browserAction.setBadgeText({text: badgeText.toString()});
 		
